43754bc6ca6bd147500cf9a4c4a0a21972d43239
dharaprayagraj <dharaprayagraj@gmail.com>
2021-06-29 06:58:37 -0400

    created base.java and data.properties, Modified in pom.xml

---
+++ src/main/java/Automation/base.java
@@ -0,0 +1,47 @@
+package Automation;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class base 
+{
+	WebDriver driver;
+	public void initializeDriver() throws IOException
+	{
+		Properties prop = new Properties(); 
+		FileInputStream fis = new FileInputStream("C:\\Users\\dhara\\eclipse-workspace\\V2Kart\\src\\main\\java\\Automation\\data.properties");
+		
+		prop.load(fis);
+		String browserName = prop.getProperty("browser");
+		
+		if(browserName=="chrome")
+		{
+			//executes in chrome browser
+			System.setProperty("webdriver.chrome.driver", prop.getProperty("chromePath"));
+			driver = new ChromeDriver();
+		}
+		else if(browserName=="firefox")
+		{
+			//executes in firefox browser
+			System.setProperty("webdriver.geko.driver", prop.getProperty("firefoxPath"));
+			driver = new FirefoxDriver();
+			
+		}
+		else if(browserName=="IE")
+		{
+			//executes in internet explorer
+			System.setProperty("webdriver.ie.driver", prop.getProperty("internetExplorerPath"));
+			driver = new InternetExplorerDriver();
+		}
+		
+		
+	}
+
+}
